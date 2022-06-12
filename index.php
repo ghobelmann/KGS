334945c125b5b297abf080742986801006f00921
Greg Hobelmann <greghobelmann@gmail.com>
2022-06-11 20:05:03 -0500

    git add *

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
 <head>
   <title>KansasGolfScores.com</title>
 
+
+  <!-- Git Hub Test -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
